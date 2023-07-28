f1f95a52ddfd59988ea30497fcda5ce0a0e01144
Madalena Machado Rocha <rochamada1997m@gmail.com>
2023-07-27 23:05:56 -0300

    :sparkles: add section home

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  main {
+    padding: 0 2.8rem;
+  }
+
+  #home {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 3.2rem;
+
+    text-align: center;
+    margin-top: 6.4rem;
+  }
+
+  .profile {
+    width: 20rem;
+    height: 20rem;
+  }
+
+  .profile img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: top;
+    
+    padding: 3.7px;
+    border: 4px solid ${({ theme }) => theme.COLORS.PINK};
+    border-radius: 50%;
+  }
+
+  h1 {
+    font-size: 3.4rem;
+  }
+
+  h1 span {
+    color: ${({ theme }) => theme.COLORS.PINK};
+  }
+
+  .links {
+    display: flex;
+    justify-content: center;
+    margin-top: 3.2rem;
+    gap: 1.6rem;
+  }
+
+  .links a {
+    display: flex;
+    align-items: center;
+    gap: 0.8rem;
+    
+    width: fit-content;
+    padding: 0.8rem 1.6rem;
+    border: 1px solid ${({ theme }) => theme.COLORS.GRAY_200};
+    border-radius: 0.8rem;
+  }
+
+  @media (min-width: 1024px) {
+    main {
+      padding: 0 12.3rem;
+    }
+    
+    #home {
+      flex-direction: row;
+      text-align: left;
+    }
+
+    .links {
+      justify-content: left;
+    }
+  }
+`;
--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,49 @@
 import { useState } from 'react';
 import { useMediaQuery } from 'react-responsive'
 
+import { Container } from './App.js'
+
 import { Header } from './components/Header'
 import { Menu } from './components/Menu'
+
+import { LinkedinLogo, GithubLogo } from '@phosphor-icons/react';
 
 function App() {
   const isDesktop = useMediaQuery({ minWidth: 1024 });
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <>
+    <Container>
       <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
       
       {!isDesktop && (
         <Menu isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
       )}
 
-      <h1>Olá! Sou Madalena, Desenvolvedora Front-end.</h1>
-    </>
+      <main>
+        <section id="home">
+          <div className="profile">
+            <img src="https://github.com/madalena-rocha.png" alt="Mulher de pele parda e cabelos castanhos lisos, usando uma camiseta preta e sorrindo." />
+          </div>
+
+          <div>
+            <h1>Olá! Sou <span>Madalena</span>, <br></br> Desenvolvedora Front-end.</h1>
+
+            <div className="links">
+              <a href="https://www.linkedin.com/in/madalena-machado-rocha/" target="_blank">
+                <LinkedinLogo size={24} />
+                LinkedIn
+              </a>
+
+              <a href="https://github.com/madalena-rocha" target="_blank">
+                <GithubLogo size={24} />
+                GitHub
+              </a>
+            </div>
+          </div>
+        </section>
+      </main>
+    </Container>
   )
 }
 
